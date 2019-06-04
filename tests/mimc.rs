520ea36522c7b96d47c3df5a89c2ebd0394042d5
Alex Vlasov <alex.m.vlasov@gmail.com>
2019-06-04 16:31:21 +0300

    benchmark on 2 million constraints

--- tests/mimc.rs
+++ tests/mimc.rs
@@ -42,7 +42,7 @@ use bellman_ce::groth16::{
 };
 
 // const MIMC_ROUNDS: usize = 322;
-const MIMC_ROUNDS: usize = 100000;
+const MIMC_ROUNDS: usize = 1000000;
 
 /// This is an implementation of MiMC, specifically a
 /// variant named `LongsightF322p3` for BLS12-381.
