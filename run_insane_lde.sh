1d71973419f72019f19525b26bce86b434c33848
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-08-20 16:02:45 +0300

    run everything using asm backend

--- run_insane_lde.sh
+++ run_insane_lde.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cargo test --release -- --ignored --nocapture test_insane_size_lde
+RUSTFLAGS="-C target-cpu=native -C target_feature=+bmi2,+adx,+sse4.1" cargo +nightly test --release --features "asm" -- --ignored --nocapture test_insane_size_lde
