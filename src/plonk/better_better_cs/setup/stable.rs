71b0ad903a3a7d9f31495e535cfa0b0e97740796
saitima <si@matterlabs.dev>
2022-09-23 21:41:55 +0300

    not deserialize marker field of vk

--- src/plonk/better_better_cs/setup/stable.rs
+++ src/plonk/better_better_cs/setup/stable.rs
@@ -20,7 +20,7 @@ use std::io::{Read, Write};
 
 use crate::plonk::better_cs::keys::*;
 
-#[derive(Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct Setup<E: Engine, C: Circuit<E>> {
     pub n: usize,
     pub num_inputs: usize,
@@ -158,7 +158,7 @@ pub struct VerificationKey<E: Engine, C: Circuit<E>> {
     pub non_residues: Vec<E::Fr>,
     pub g2_elements: [E::G2Affine; 2],
 
-    #[serde(skip_serializing, default)]
+    #[serde(skip_serializing,skip_deserializing, default)]
     #[serde(bound(serialize = ""))]
     #[serde(bound(deserialize = ""))]
     _marker: std::marker::PhantomData<C>
